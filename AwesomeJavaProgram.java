6b09006d39e791dae290bcfe613f1d251a556c40
jpaulgb <jepgb@hotmail.com>
2025-09-25 15:15:06 -0500

    conditional if of String

--- AwesomeJavaProgram.java
+++ AwesomeJavaProgram.java
@@ -3,11 +3,22 @@ import java.util.logging.MemoryHandler;
 public class AwesomeJavaProgram {
 
     public static void main(String[] args) {
-         //Llamando a burp()
-        System.out.println(printName("John", 2));
+        String name = printName("Larry", 2);
     }
 
 private static String printName(String name, int number) {
+    
+    //conditional
+    if (name.equals("John")) {
+        System.out.println("This guy is awesome");
+    }
+    else if (name.equals("Larry")) {
+        System.out.println("This guy is Ok");
+    }
+    else {
+
+    }
+    
     return("My name is " + name);
 }
 
