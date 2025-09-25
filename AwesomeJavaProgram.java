90dce4695d58ff098d161c73045cea185dc47dd7
jpaulgb <jepgb@hotmail.com>
2025-09-25 14:40:50 -0500

    agregando datos primitivos y no primitivos

---
+++ AwesomeJavaProgram.java
@@ -0,0 +1,29 @@
+import java.util.logging.MemoryHandler;
+
+public class AwesomeJavaProgram {
+
+    public static void main(String[] args) {
+        //Datos Primitivos:
+
+        //Entero
+        int myInt = 7;
+        
+        //Números de punto flotante:
+        double shoeSize = 5;
+
+        //Carácter
+        char myInitial = 'J';
+
+        //Datos No Primitivos
+        //Secuencias de caracteres
+        String myName = "Jhon";
+
+        double result = myInt * shoeSize;   
+        
+        //Multiplicación
+        System.out.println(myInt * shoeSize);
+        System.out.println(result);
+        
+
+    }
+}
